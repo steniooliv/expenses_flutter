8210318b374934eb0c6855d66fd722622c0f013b
Stenio Oliveira <steniooliv@gmail.com>
2021-04-14 21:44:25 -0300

    import and map transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses_flutter/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -12,15 +13,46 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transaction = [
+    Transaction(
+      id: "t1",
+      title: "New Shoes",
+      value: 300.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "New T-Shirt",
+      value: 80.99,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Personal Expenses"),
-      ),
-      body: Center(
-        child: Text("Inicial Version"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Personal Expenses"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text("Graphic"),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: _transaction.map((e) {
+                return Card(
+                  child: Text(e.title),
+                );
+              }).toList(),
+            )
+          ],
+        ));
   }
 }
