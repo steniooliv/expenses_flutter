71a0d5b7726a006f8f2d5988fcca6a84fdac9506
Stenio Oliveira <steniooliv@gmail.com>
2021-04-15 21:48:40 -0300

    create button for add transaction

--- lib/main.dart
+++ lib/main.dart
@@ -16,23 +16,35 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Personal Expenses"),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                child: Card(
-                  color: Colors.blue,
-                  child: Text("Graphic"),
-                  elevation: 5,
-                ),
-              ),
-              TransactionUser(),
-            ],
+      appBar: AppBar(
+        title: Text("Personal Expenses"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
           ),
-        ));
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text("Graphic"),
+                elevation: 5,
+              ),
+            ),
+            TransactionUser(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
