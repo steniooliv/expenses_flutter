2c9cd3104400875084ce9130a0a2697ec738c1b4
Stenio Oliveira <steniooliv@gmail.com>
2021-04-15 23:56:59 -0300

    close modal after submit

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,8 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _transaction.add(newTransaction);
     });
+
+    Navigator.of(context).pop();
   }
 
   _openTransactionFormModal(BuildContext context) {
